bf54325add3c2a6420efa5ef9d0517af4d0d8c94
Adrian Lozano <adrian@xenotype.com>
2017-12-01 17:01:16 +0100

    Use datetime2 instead

--- RiderAspNetMvc/DataAccess/AppContext.cs
+++ RiderAspNetMvc/DataAccess/AppContext.cs
@@ -9,6 +9,19 @@ namespace RiderAspNetMvc.DataAccess {
 
         public static AppDbContext Create() => new AppDbContext();
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            // Override the default precision of two decimals so that coordinates can be stored.
+            //modelBuilder.Entity<Studio>().Property(s => s.Latitude).HasPrecision(18, 15);
+            //modelBuilder.Entity<Studio>().Property(s => s.Longitude).HasPrecision(18, 15);
+            
+            // SQL Server's datetime2 has the same range and precision as .NET's DateTime
+            modelBuilder.Properties<System.DateTime>().Configure(c => c.HasColumnType("datetime2"));
+
+        }
+
+
         public DbSet<Student> Students { get; set; }
         public DbSet<Course> Courses { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
