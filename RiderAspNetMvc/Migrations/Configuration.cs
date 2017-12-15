340a77d11b0e73b807f211bc345e0e0d88bffdab
Adrian Lozano <adrian@xenotype.com>
2017-12-15 15:49:02 +0100

    Message handling in seed

--- RiderAspNetMvc/Migrations/Configuration.cs
+++ RiderAspNetMvc/Migrations/Configuration.cs
@@ -17,17 +17,32 @@ namespace RiderAspNetMvc.Migrations
 
         protected override void Seed(DataAccess.AppDbContext db)
         {
+            List<string> log = new List<string>();
+            try
+            {
+                SeedAll(db, log);
+            }
+            catch (Exception e)
+            {
+                throw new Exception(string.Join("\n", log), e);
+            }
+            throw new Exception(string.Join("\n", log));
+
+        }
+
+        private static void SeedAll(DataAccess.AppDbContext db, List<string> log)
+        {
             var firstNames = new[] {
-                "Adelia", "Alba", "Alden", "Alessandra", "Alfreda", "Alisa", "Allene", "Allene", "Amber", "Ardella", "Ardelle", "Aretha", "Asuncion", "Belia", "Billie", "Branda", "Breann", "Brigida", "Britany", "Britteny", "Brittney", "Candi", "Carly", "Celsa", "Charles", "Charlie", //"Christina", "Chung", "Cinda", "Claire", "Clemente", "Cliff", "Corey", "Corliss", "Cortez", "Cyndi", "Damien", "Damion", "Danita", "Davida", "Denese", "Denise", "Deonna", "Diann", "Digna", "Dorian", "Dot", "Drema", "Edgar", "Edith", "Elayne", "Elfreda", "Elicia", "Else", "Emilie", "Emmy", "Faviola", "Felisa", "Freida", "Frida", "Gabriel", "Gala", "Geneva", "Gertha", "Ginny", "Gwenda", "Gwenda", "Ha", "Hal", "Herb", "Herman", "Hyon", "Inocencia", "Jane", "Jeanelle", "Jenice", "Jenine", "Jetta", "Joe", "Joesph", "Johnathan", "Johnie", "Joi", "Joshua", "Julietta", "Kaci", "Kanisha", "Karoline", "Karoline", "Kati", "Katrina", "Kaylee", "Kenyatta", "Kerrie", "Klara", "Kristie", "Lamont", "Larue", "Lashanda", "Lashandra", "Laticia", "Lauretta", "Leandra", "Lenna", "Leonie", "Letisha", "Lili", "Linh", "Linsey", "Lorelei", "Louise", "Lucina", "Marcelene", "Marshall", "Maryanne", "Maud", "Mckenzie", "Meghann", "Minna", "Minna", "Miss", "Mitsuko", "Monroe", "Mose", "Muriel", "My", "Myrna", "Nada", "Nannette", "Nena", "Nga", "Nicol", "Nu", "Ona", "Onie", "Patience", "Patria", "Patricia", "Pinkie", "Porfirio", "Queenie", "Rana", "Raul", "Reed", "Richie", "Rita", "Rocio", "Romelia", "Romeo", "Ronnie", "Rosana", "Ruben", "Ryan", "Sanda", "Santa", "Sara", "Sara", "Shanae", "Shane", "Sharonda", "Sheena", "Shelia", "Shelton", "Shirlene", "Siobhan", "Siu", "Sixta", "Skye", "Slyvia", "Sophia", "Stacie", "Stefania", "Stormy", "Suzanna", "Tad", "Talisha", "Teodoro", "Terrie", "Theda", "Theo", "Thresa", "Tomi", "Tonita", "Tracie", "Trinh", "Trudi", "Tuan", "Un", "Verdie", "Verona", "Vita", "Viviana", "Wai", "Wendy", "Wilfred", "Wilhemina", "Willene", "Wynona", "Yadira", "Zonia", 
+                "Adelia", "Alba", "Alden", "Alessandra", "Alfreda", "Alisa", "Allene", "Allene", "Amber", "Ardella", "Ardelle", "Aretha", "Asuncion", "Belia", "Billie", "Branda", "Breann", "Brigida", "Britany", "Britteny", "Brittney", "Candi", "Carly", "Celsa", "Charles", "Charlie", "Christina", "Chung", "Cinda", "Claire", "Clemente", "Cliff", "Corey", "Corliss", "Cortez", "Cyndi", "Damien", "Damion", "Danita", "Davida", "Denese", "Denise", "Deonna", "Diann", "Digna", "Dorian", "Dot", "Drema", "Edgar", "Edith", "Elayne", "Elfreda", "Elicia", "Else", "Emilie", "Emmy", "Faviola", "Felisa", "Freida", "Frida", "Gabriel", "Gala", "Geneva", "Gertha", "Ginny", "Gwenda", "Gwenda", "Ha", "Hal", "Herb", "Herman", "Hyon", "Inocencia", "Jane", "Jeanelle", "Jenice", "Jenine", "Jetta", "Joe", "Joesph", "Johnathan", "Johnie", "Joi", "Joshua", "Julietta", "Kaci", "Kanisha", "Karoline", "Karoline", "Kati", "Katrina", "Kaylee", "Kenyatta", "Kerrie", "Klara", "Kristie", "Lamont", "Larue", "Lashanda", "Lashandra", "Laticia", "Lauretta", "Leandra", "Lenna", "Leonie", "Letisha", "Lili", "Linh", "Linsey", "Lorelei", "Louise", "Lucina", "Marcelene", "Marshall", "Maryanne", "Maud", "Mckenzie", "Meghann", "Minna", "Minna", "Miss", "Mitsuko", "Monroe", "Mose", "Muriel", "My", "Myrna", "Nada", "Nannette", "Nena", "Nga", "Nicol", "Nu", "Ona", "Onie", "Patience", "Patria", "Patricia", "Pinkie", "Porfirio", "Queenie", "Rana", "Raul", "Reed", "Richie", "Rita", "Rocio", "Romelia", "Romeo", "Ronnie", "Rosana", "Ruben", "Ryan", "Sanda", "Santa", "Sara", "Sara", "Shanae", "Shane", "Sharonda", "Sheena", "Shelia", "Shelton", "Shirlene", "Siobhan", "Siu", "Sixta", "Skye", "Slyvia", "Sophia", "Stacie", "Stefania", "Stormy", "Suzanna", "Tad", "Talisha", "Teodoro", "Terrie", "Theda", "Theo", "Thresa", "Tomi", "Tonita", "Tracie", "Trinh", "Trudi", "Tuan", "Un", "Verdie", "Verona", "Vita", "Viviana", "Wai", "Wendy", "Wilfred", "Wilhemina", "Willene", "Wynona", "Yadira", "Zonia", 
             };
 
             var lastNames = new[] {
-                "Abalos", "Ader", "Adolphson", "Alderman", "Alverez", "Ammann", "Angles", "Anstett", "Aune", "Barge", "Beauvais", "Benningfield", "Bess", // "Blessing", "Boddy", "Boney", "Brace", "Bridge", "Brunetti", "Buckles", "Bulman", "Byrum", "Campuzano", "Canterbury", "Chavers", "Clinkscales", "Coney", "Connor", "Cowan", "Crosier", "Darland", "Desmond", "Dolphin", "Dudley", "Edney", "Finamore", "Flynt", "Fontanez", "Fore", "Fuell", "Galindo", "Gehrke", "Gildea", "Gosser", "Guercio", "Hamil", "Hansell", "Higdon", "Hoard", "Hochstetler", "Holliday", "Holmen", "Keen", "Kimura", "Kivett", "Klemp", "Lambros", "Lanahan", "Lebouef", "Lobb", "Lundy", "Manning", "Maresca", "Markovich", "Masiello", "Matherne", "Mccallister", "Mcgonigle", "Mudge", "Neely", "Nish", "Orcutt", "Paik", "Panek", "Pardon", "Pilkington", "Posner", "Putman", "Rand", "Rasnick", "Reich", "Schalk", "Seagraves", "Sedberry", "Shillings", "Shipman", "Siegler", "Snuggs", "Stanley", "Stowe", "Styers", "Sylva", "Tardy", "Thiede", "Tobias", "Vigliotti", "Whitcher", "Wilcher", "Wolcott", "Yokoyama",
+                "Abalos", "Ader", "Adolphson", "Alderman", "Alverez", "Ammann", "Angles", "Anstett", "Aune", "Barge", "Beauvais", "Benningfield", "Bess", "Blessing", "Boddy", "Boney", "Brace", "Bridge", "Brunetti", "Buckles", "Bulman", "Byrum", "Campuzano", "Canterbury", "Chavers", "Clinkscales", "Coney", "Connor", "Cowan", "Crosier", "Darland", "Desmond", "Dolphin", "Dudley", "Edney", "Finamore", "Flynt", "Fontanez", "Fore", "Fuell", "Galindo", "Gehrke", "Gildea", "Gosser", "Guercio", "Hamil", "Hansell", "Higdon", "Hoard", "Hochstetler", "Holliday", "Holmen", "Keen", "Kimura", "Kivett", "Klemp", "Lambros", "Lanahan", "Lebouef", "Lobb", "Lundy", "Manning", "Maresca", "Markovich", "Masiello", "Matherne", "Mccallister", "Mcgonigle", "Mudge", "Neely", "Nish", "Orcutt", "Paik", "Panek", "Pardon", "Pilkington", "Posner", "Putman", "Rand", "Rasnick", "Reich", "Schalk", "Seagraves", "Sedberry", "Shillings", "Shipman", "Siegler", "Snuggs", "Stanley", "Stowe", "Styers", "Sylva", "Tardy", "Thiede", "Tobias", "Vigliotti", "Whitcher", "Wilcher", "Wolcott", "Yokoyama",
             };
 
             var fnLength = firstNames.Length;
             var lnLength = lastNames.Length;
-            var skip = lnLength * fnLength / 1000 - 1;
+            var skip = Math.Ceiling(lnLength / 4.0) + 1;
 
             var students = lastNames
                 .SelectMany(last => firstNames.Select(first => new Student
@@ -38,7 +53,6 @@ namespace RiderAspNetMvc.Migrations
                 .Where((s, i) => i % skip == 0)
                 .Distinct()
                 .ToArray();
-
 
             db.Students.AddOrUpdate(s => new { s.FirstName, s.LastName }, students);
             db.SaveChanges();
@@ -91,30 +105,32 @@ namespace RiderAspNetMvc.Migrations
 
                 enrollmentCount++;
                 int courseIndex = enrollmentCount % courses.Length;
-                //                if (courseIndex == courses.Length - 1) continue; // Last course is empty            
+                if (courseIndex == courses.Length - 1) continue; // Last course is empty            
 
                 course = courses[courseIndex];
-                //throw new Exception($"{courseIndex}/{courses.Length} {course.Name}");
-                //throw new Exception($"{studentIndex}/{students.Length} {student.Name}");
 
-                string debug
-                    = $"{studentIndex}/{students?.Length} ({ student?.Name }), { nameof(student) } is null\n"
-                    + $"{courseIndex}/{courses?.Length} ({ course?.Name }), { nameof(course) } is null\n";
+                log.Add($"{studentIndex}/{students?.Length} ({ student?.Name })   " +
+                    $"{courseIndex}/{courses?.Length} ({ course?.Name })");
 
-                if (student == null) { throw new Exception(debug); }
-                if (course == null) { throw new Exception(debug); }
+                if (student == null) log.Add("Student is null");
+                if (course == null) log.Add("Course is null");
 
                 var e = new Enrollment { CourseId = course.Id, StudentId = student.Id };
-
-                //throw new Exception($"{e?.Student?.Name} ({e?.Course?.Name})");
-                //throw new Exception(enrollments?.Count().ToString());
 
                 enrollments.Add(e);
             }
             //db.Enrollments.AddOrUpdate
+            log.Add(
+                string.Join("\n\n",
+                    "Done!",
+                    string.Join("\n", students.Select(s => s.Name)),
+                    string.Join("\n", courses.Select(c => c.Name)),
+                    string.Join("\n", enrollments.Select(e => $"{e.CourseId}:{e.StudentId}")),
+                    $"{students.Length} + students",
+                    $"{courses.Length} + courses"
 
+                    ));
         }
-
 
     }
 }
